7d4a9edb53f5b774913a3b092471da855c7b3d41
RobinKrekeler <=>
2020-06-24 09:49:59 +0200

    Add european eez cutout.

--- reg/create_rectuangular.py
+++ reg/create_rectuangular.py
@@ -1,4 +1,6 @@
 """Create rectangular shapefile as scope for GLAES priors."""
+import pandas as pd
+import geopandas as gpd
 import shapely.geometry as sp
 import fiona
 import fiona.crs
@@ -36,3 +38,44 @@ with fiona.open(OUT_DIR, 'w', 'ESRI Shapefile', SCHEMA, crs=CRS) as c:
              'properties': {'id': 'scope'}})
 
 
+#%%
+# =============================================================================
+# Test: Mediterrainean sea
+# =============================================================================
+med_sea = sp.box(4E6, 3.3E6, 4.3E6, 3.6E6)  # xl, yl, xu, yu
+
+CRS = fiona.crs.from_epsg(3035)
+
+OUT_DIR = 'C:/users/Robin/Git_Projects/glaes/reg/north_sea_rectangular.shp'
+
+with fiona.open(OUT_DIR, 'w', 'ESRI Shapefile', SCHEMA, crs=CRS) as c:
+    c.write({'geometry': sp.mapping(north_sea),
+             'properties': {'id': 'scope'}})
+
+#%%
+# =============================================================================
+# Europe EEZ
+# =============================================================================
+
+OUT_DIR = 'C:/users/Robin/Git_Projects/glaes/reg/europe_eez_rectangular.shp'
+
+# read shp files
+eez = gpd.read_file('C:/Users/Robin/Documents/Inatech/Model/input_raw/Marineregions/eez_v11.shp')
+europe_rectangular = gpd.read_file('C:/users/Robin/Git_Projects/glaes/reg/europe_rectangular.shp')
+europe = pd.read_csv('C:/Users/Robin/Documents/Inatech/Model/input_raw/ISO/country_codes.csv',
+                     encoding='ISO-8859-1')
+
+# filter EEZ of european countries
+europe = europe[europe['region'] == 'Europe']['alpha-3']
+europe = europe[europe != 'RUS']
+europe_eez = eez[eez['ISO_SOV1'].isin(europe)].cx[-44:75, 30:75]
+
+# unify european EEZs
+europe_eez = europe_eez['geometry'].unary_union
+europe_eez = gpd.GeoDataFrame({'geometry': europe_eez}, crs=eez.crs)
+
+# common CRS
+europe_eez = europe_eez.to_crs(europe_rectangular.crs)
+
+europe_eez_rectangular = gpd.overlay(europe_rectangular,europe_eez, how='intersection')
+europe_eez_rectangular.to_file(OUT_DIR)
